bd39b72c50e6360bcfa2e1c7a69f1e683ee8189d
Neeraj6264 <161701889+Neeraj6264@users.noreply.github.com>
2025-03-25 00:19:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const fileInput = promptForm.querySelector("#file-input");
 const fileUploadWrapper = promptForm.querySelector(".file-upload-wrapper");
 const themeToggleBtn = document.querySelector("#theme-toggle-btn");
 // API Setup
-const API_KEY = "AIzaSyBbRTxvkbbROfCSskfC-HsUIOnO7isZ-Go";//
+const API_KEY = "";//
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 let controller, typingInterval;
 const chatHistory = [];
